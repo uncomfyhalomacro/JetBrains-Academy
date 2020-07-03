7f36f25eaa43c93a3d3808a2a32088f6b579cf1e
ReneganRonin <66054069+ReneganRonin@users.noreply.github.com>
2020-07-03 17:38:52 +0800

    Update Coffee Machine (Fixed).py
    Fixing issues on the while-loop and created more consistency for the output lines.

--- Python Adventures/Coffee Machine/Revisions/Coffee Machine (Fixed).py
+++ Python Adventures/Coffee Machine/Revisions/Coffee Machine (Fixed).py
@@ -31,33 +31,36 @@ class CoffeeMachine:
         return CoffeeMachine.buy(self)
 
     def fill(self):
-        fill_water = int(input('How much ml water?\n'))
+        fill_water = int(input('\nHow much ml water?\n'))
         fill_milk = int(input('How much ml milk?\n'))
         fill_beans = int(input('How much grams of coffee beans?\n'))
-        fill_cups = int(input('How many disposable cups?\n\n'))
+        fill_cups = int(input('How many disposable cups?\n'))
         self.ml_water += fill_water
         self.ml_milk += fill_milk
         self.coffee_beans += fill_beans
         self.disp_cups += fill_cups
+        print('')
         CoffeeMachine.action(self)
 
     def action(self) -> object:
-        user_action = input("What do you want to do? -Buy- -Fill- -Take- -Remaining- -Exit-")
+        user_action = input("What do you want to do? -Buy- -Fill- -Take- -Remaining- -Exit-\n")
         while True:
             if user_action == 'Buy':
                 CoffeeMachine.buy(self)
             elif user_action == "Fill":
                 CoffeeMachine.fill(self)
             elif user_action == "Take":
-                print(f'You get ${self.money}.')
+                print(f'You get ${self.money}.\n')
                 self.money -= self.money
+                CoffeeMachine.action(self)
+                break
             elif user_action == 'Remaining':
                 print(CoffeeMachine.__str__(self))
                 CoffeeMachine.action(self)
             elif user_action == 'Exit':
                 exit()
             else:
-                print("No such input. Restarting coffee machine.")
+                print("No such input. Restarting coffee machine.\n")
                 CoffeeMachine.action(self)
 
     def latte(self):
